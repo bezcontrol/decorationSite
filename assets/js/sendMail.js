27cbedc8dbf69f4c1a8ef51eaca8887b02a4d040
Ruslan Volodko <ruslan_volodko@epam.com>
2019-11-26 22:34:36 +0200

    add clearing mail form fields

--- assets/js/sendMail.js
+++ assets/js/sendMail.js
@@ -2,22 +2,26 @@ document.getElementById("senderMail").addEventListener("click", sendEmail, false
 
 function sendEmail(){
 
-	let firstName = document.getElementById('firstName').value;
-	let lastName = document.getElementById('lastName').value;
-	let email = document.getElementById('mail').value;
+	let firstName = document.getElementById('firstName');
+	let lastName = document.getElementById('lastName');
+	let email = document.getElementById('mail');
+        let emptyString = "";
 
 	(function(){
 		emailjs.init("user_uA3tJqibLCPfQlnTJGdGD"); //Insert your user ID 
 	})();
 
 	var templateParams = {
-		name: 'email',
-		notes: firstName + " " + lastName + 'want to make an order!'
+		name: email.value,
+		notes: firstName.value + " " + lastName.value + 'want to make an order!'
 	};
 
 	emailjs.send('gmail', 'template_4LeO82KY', templateParams) //Insert your email service ID and email template ID
 	.then(function(response) {
 		console.log('SUCCESS!', response.status, response.text);
+                email.value = emptyString ;
+                lastName.value = emptyString;
+                firstName.value = emptyString;
 	}, function(error) {
 		console.log('FAILED...', error);
 	});
